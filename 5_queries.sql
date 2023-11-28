d3110900b45b8e9aa19640defa80bde8e7e6f289
Felix Reiniger <felix.reiniger@ost.ch>
2023-11-28 11:37:37 +0100

    rename

--- 5_queries.sql
+++ 5_queries.sql
@@ -67,8 +67,8 @@ SELECT
   student.first_name,
   student.last_name,
   rating.rating,
-  lecturer.first_name AS 'Lecturer First_name'
-  lecturer.last_name AS 'Lecturer Last_name'
+  lecturer.first_name AS Lfirst_name
+  lecturer.last_name AS Llast_name
 FROM 
     student
 JOIN rating ON student.matriculation_no = rating.student
